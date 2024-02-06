38bcdd47516f2afcd6d247ad594fe8c7df07c6ef
ImprovingAnna <anna.mayer@improving.com>
2024-02-06 12:39:48 -0600

    added events

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Image } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 
@@ -18,8 +18,18 @@ const Events = () => {
                 </Col>
             </Row>
             <Row className="m-5">
-                <Col>
-                    <p>No upcoming events currently scheduled.</p>
+                <h2 className="fs-1">February</h2>
+            </Row>
+            <Row className="m-5">
+                <Col className="justify-content-center">
+                    <a href="https://fb.me/e/49hBRFrEI" target="_blank" rel="noreferrer" className="text-decoration-none text-light text-center">
+                        <Image height={300} src="https://acrdphotos.blob.core.windows.net/photos/trivia.png" alt="trivia"></Image>
+                    </a>
+                </Col>
+                <Col className="justify-content-center">
+                    <a href="https://fb.me/e/3CCWbIY2M" target="_blank" rel="noreferrer" className="text-decoration-none text-light text-center">
+                        <Image height={300} src="https://acrdphotos.blob.core.windows.net/photos/Feb_2024_FB_event.jpg" alt="season opener"></Image>
+                    </a>
                 </Col>
             </Row>
         </Container>
