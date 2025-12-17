fee2ae4b4f51204443b27e662fb5c8c29ee1e1e2
UKillLele <mayerannam@gmail.com>
2025-12-16 23:33:45 -0600

    attempt to get api to push

--- api/src/functions/usps.ts
+++ api/src/functions/usps.ts
@@ -1,30 +1,30 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions';
+// import { app, HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions';
 
-export async function validateAddress(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-  if (request) {
-    const address = await request.json() as Address;
+// export async function validateAddress(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+//   if (request) {
+//     const address = await request.json() as Address;
     
-  }
+//   }
 
-  return {
-      status: 404,
-      body: 'Please enter an address',
-  };
-}
+//   return {
+//       status: 404,
+//       body: 'Please enter an address',
+//   };
+// }
 
-app.http('validateAddress', {
-    methods: ['POST'],
-    authLevel: 'function',
-    route: 'validate-address',
-    handler: validateAddress,
-});
+// app.http('validateAddress', {
+//     methods: ['POST'],
+//     authLevel: 'function',
+//     route: 'validate-address',
+//     handler: validateAddress,
+// });
 
-class Address
-    {
-        address1: string;
-        address2: string;
-        city: string;
-        state: string;
-        zipcode: string;
-        returnText: string;
-    }
+// class Address
+//     {
+//         address1: string;
+//         address2: string;
+//         city: string;
+//         state: string;
+//         zipcode: string;
+//         returnText: string;
+//     }
